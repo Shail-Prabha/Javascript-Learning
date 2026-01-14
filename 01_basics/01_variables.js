00662b9663512b278b12f7f52ec66fe2eec71568
Shail-Prabha <shailprabha2005@gmail.com>
2026-01-14 14:59:24 +0000

    Main

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accounEmail = "shailprabha@gmail.com"
+var accountPassword = "12345"
+let accountState;
+// console.table([accountId , accounEmail , accountPassword , accountState]);
+// console.log([accountId , accounEmail , accountPassword , accountState]);
+
+
+
+// accountId = "76567"
+// accounEmail = "sh@gmail.com"
