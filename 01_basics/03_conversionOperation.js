7444866decbda748c1ca814e212b9b8e5b9307a7
Shail-Prabha <shailprabha2005@gmail.com>
2026-01-15 10:38:19 +0000

    Conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,7 +1,7 @@
-let score = "33abc"
-let score1 = null
-let score2 = undefined
-let score3 = true
+// let score = "33abc"
+// let score1 = null
+// let score2 = undefined
+// let score3 = true
 
 // console.log( typeof score);
 // console.log( typeof (score));
@@ -42,7 +42,57 @@ let score3 = true
 // "" => false
 // "shail" => true
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber);
+
+
+
+// *********************** Operations ********************** // 
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Shail"
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+// gameCounter++;
+++gameCounter
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
