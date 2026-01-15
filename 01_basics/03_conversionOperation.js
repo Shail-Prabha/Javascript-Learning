3cc0af0eb28c31b21a7e614af22b305f508da31b
Shail-Prabha <shailprabha2005@gmail.com>
2026-01-15 10:03:24 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,48 @@
+let score = "33abc"
+let score1 = null
+let score2 = undefined
+let score3 = true
+
+// console.log( typeof score);
+// console.log( typeof (score));
+// console.log(score1);
+// console.log(score3);
+
+
+
+// let valueInNumber = Number(score)
+// console.log( typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let valueInNumber = Number(score1)
+// console.log( typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let valueInNumber = Number(score2)
+// console.log( typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let valueInNumber = Number(score3)
+// console.log( typeof valueInNumber);
+// console.log(valueInNumber);
+
+/* notes */
+// "33" => 33
+// "33abc" => NaN
+// true =>1; false => 0
+
+
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "shail" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+
