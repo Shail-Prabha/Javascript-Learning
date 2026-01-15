9288d9cb32feaf887034fbbbf2593e0022923b40
Shail-Prabha <shailprabha2005@gmail.com>
2026-01-15 09:29:53 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict" //treat all js code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+let name = "Shail Prabha"
+let age = 18
+let isLoggedIn = false
+let state;
+
+/* primitive*/
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null -> standalone value
+// undefined => value is not assigned
+// symbol -> for uniqueness
+
+
+// object 
+console.log(typeof "Shail Prabha");
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof state);
+console.log(typeof undefined);//undefined
+
+
