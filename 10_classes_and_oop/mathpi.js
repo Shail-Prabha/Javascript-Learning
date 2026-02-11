957314da407e591fd101c5a66c416622391a3eba
Shail-Prabha <shailprabha2005@gmail.com>
2026-02-11 16:35:03 +0530

    mathpi

---
+++ 10_classes_and_oop/mathpi.js
@@ -0,0 +1,31 @@
+ const descriptor = Object.getOwnPropertyDescriptor(Math, "PI")
+
+ console.log(descriptor);
+ 
+
+// console.log(Math.PI);
+// Math.PI = 5
+// console.log(Math.PI);
+
+const course = {
+    name: "Mathematics",
+    price: 250,
+    isAvailable: true
+}
+console.log(Object.getOwnPropertyDescriptor(course, "name"));
+
+
+Object.defineProperty(course, 'name', {
+//     writable: false,
+    enumerable: true,
+//     configurable: false
+})
+console.log(Object.getOwnPropertyDescriptor(course, "name"));
+
+for(let [key, value] of Object.entries(course)) {
+    if(typeof value !== 'function') {
+        console.log(`${key} : ${value}`);
+    }    
+}
+
+
